fa59fa04891c878ab8f4aec11aa0e6c36c73cffa
HPhucDev <hoangphucdeveloper@gmail.com>
2022-12-05 23:10:07 +0700

    Add CRUD ClassController

--- src/main/java/com/hcmute/management/repository/UserRepository.java
+++ src/main/java/com/hcmute/management/repository/UserRepository.java
@@ -3,10 +3,11 @@ package com.hcmute.management.repository;
 import com.hcmute.management.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import java.util.Optional;
 import java.util.UUID;
-
+@EnableJpaRepositories
 public interface UserRepository extends JpaRepository<UserEntity, String> {
     @Query(value = "SELECT * From users where user_name=?1 limit 1",nativeQuery = true)
 Optional<UserEntity> findByUserName(String username);
