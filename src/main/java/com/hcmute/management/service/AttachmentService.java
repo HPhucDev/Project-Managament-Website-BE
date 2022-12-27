19a9c22d78e6bd022481bbfec8ea3df2db370f8c
HPhucDev <hoangphucdeveloper@gmail.com>
2022-12-27 22:32:00 +0700

    Add getAllSubjectAttachment

--- src/main/java/com/hcmute/management/service/AttachmentService.java
+++ src/main/java/com/hcmute/management/service/AttachmentService.java
@@ -3,6 +3,7 @@ package com.hcmute.management.service;
 import com.hcmute.management.handler.FileNotImageException;
 import com.hcmute.management.model.entity.AttachmentEntity;
 import com.hcmute.management.model.entity.ProgressEntity;
+import com.hcmute.management.model.entity.SubjectEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,4 +14,5 @@ import java.util.List;
 @Service
 public interface AttachmentService {
     void uploadFile(MultipartFile[] file, ProgressEntity progress) throws FileNotImageException;
+    List<AttachmentEntity> findAllBySubject(SubjectEntity subject);
 }
